7279aeb3afc010f2e7dc967bb117c7c13eb92144
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-10-09 21:48:45 +0200

    Add dummy comments to both packages

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,7 +2,7 @@ import { generateViteConfig } from 'dev-utils'
 
 export default generateViteConfig({
   absoluteRootDir: __dirname,
-  formats: ['es', 'cjs'],
+  formats: ['es', 'cjs'], // support both formats
   pluginCategories: ['dts'],
   isLibrary: true,
 })
