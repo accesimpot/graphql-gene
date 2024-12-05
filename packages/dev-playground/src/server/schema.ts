5602d184ba501f383c1bd4ba322b1ef297e20068
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-12-05 11:04:11 +0100

    Remove schema option from generateSchema

--- packages/dev-playground/src/server/schema.ts
+++ packages/dev-playground/src/server/schema.ts
@@ -1,15 +1,9 @@
-import {
-  DateTypeDefinition,
-  DateTimeTypeDefinition,
-  DateResolver,
-  DateTimeResolver,
-} from 'graphql-scalars'
+import { DateResolver, DateTimeResolver } from 'graphql-scalars'
 import { generateSchema } from 'graphql-gene'
 import { pluginSequelize } from '@graphql-gene/plugin-sequelize'
 import * as graphqlTypes from '../models/graphqlTypes'
 
 const { schema, schemaString, schemaHtml } = generateSchema({
-  schema: [DateTypeDefinition, DateTimeTypeDefinition].join('\n'),
   resolvers: {
     Date: DateResolver,
     DateTime: DateTimeResolver,
