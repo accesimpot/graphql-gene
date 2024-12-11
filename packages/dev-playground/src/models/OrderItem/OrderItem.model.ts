cf0f07f6b22984d9dddc7b5d3a200663634a43b7
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-12-11 15:19:30 +0100

    Use findOptions in OrderItem to filter Product

--- packages/dev-playground/src/models/OrderItem/OrderItem.model.ts
+++ packages/dev-playground/src/models/OrderItem/OrderItem.model.ts
@@ -7,8 +7,11 @@ import {
   Model,
   Table,
 } from 'sequelize-typescript'
+import { defineGraphqlGeneConfig } from 'graphql-gene'
 import { Order } from '../Order/Order.model'
 import { Product } from '../Product/Product.model'
+
+const PRODUCT_ASSOCIATION = 'product'
 
 export
 @Table
@@ -30,8 +33,26 @@ class OrderItem extends Model {
 
   @ForeignKey(() => Product)
   @Column(DataType.INTEGER)
-  declare productId: number | null
+  declare productId: number | null;
 
   @BelongsTo(() => Product)
-  declare product: Product | null
+  declare [PRODUCT_ASSOCIATION]: Product | null
+
+  static readonly geneConfig = defineGraphqlGeneConfig(OrderItem, {
+    findOptions({ findOptions }) {
+      findOptions.include = findOptions.include || []
+
+      const possibleProductOptions = findOptions.include.find(
+        opt => opt.association === PRODUCT_ASSOCIATION
+      )
+      const productOptions = possibleProductOptions || { association: PRODUCT_ASSOCIATION }
+      if (!possibleProductOptions) findOptions.include.push(productOptions)
+
+      productOptions.where = { ...productOptions.where, isPublished: true }
+
+      // By making it "required", we tell Sequelize to use an INNER JOIN, therefore exclude
+      // order items that don't have product.isPublished equal to `true`.
+      productOptions.required = true
+    },
+  })
 }
