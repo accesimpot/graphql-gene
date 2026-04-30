f4237e0ed866c8bcd5def6c06281912219473e23
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2026-04-30 14:32:08 +0200

    Add dummy comment to core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,8 @@ export {
   generateDefaultQueryFilterTypeDefs,
 } from './defaultResolver'
 
+// DUMMY COMMENT (to remove)
+
 export * from './defineConfig'
 export * from './resolvers'
 export * from './schema'
