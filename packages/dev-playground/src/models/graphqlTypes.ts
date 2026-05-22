293c7d0cec46b7dab67d56a7d5ecdd96260f8241
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2026-05-22 13:27:36 +0200

    Move test union inside Product.model.ts

--- packages/dev-playground/src/models/graphqlTypes.ts
+++ packages/dev-playground/src/models/graphqlTypes.ts
@@ -1,10 +1,6 @@
-/** Initializes Sequelize models (associations) before SDL/schema consumers import concrete types. */
-import './sequelize'
-import { defineUnion } from 'graphql-gene'
-
 export * from './models'
 
-export { ProductReviewAverage } from './Product/Product.model'
+export { ProductReviewAverage, IntegrationDemoUnion } from './Product/Product.model'
 export {
   UpdateOrderStatusOutput,
   OrderStatusEnum,
@@ -12,6 +8,3 @@ export {
   MessageTypeEnum,
   SomeOtherInput,
 } from './Order/Order.model'
-
-/** Covers graphql-gene `defineUnion` / union SDL emission in `schema.ts`. */
-export const IntegrationDemoUnion = defineUnion(['Product', 'Order'])
